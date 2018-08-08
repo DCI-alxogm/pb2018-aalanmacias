5a97b2afd08023a56105281c8365af5c5de1397b
Macias Padilla Brayhan Alan <maciaspb2018@licifug.ugto.mx>
2018-08-08 09:36:15 -0500

    eh terminado el programa del dia 8 de agosto de 2018

---
+++ semana1/programa_hola.c
@@ -0,0 +1,17 @@
+    /*
+     Esto es un comentario. Me sirve para escribir información relevante del programa que estoy haciendo. Por  ejemplo:
+     Este programa imprime la típica frase “hola mundo a la pantalla”
+     Creado el 06/08/2018 por …
+    */
+
+#include<stdio.h>
+
+int main()
+{
+
+ printf("hola Alan \n");
+ printf("Este es el primer programa del curso \n");
+
+}
+
+/*termine de escribir el programa*/
