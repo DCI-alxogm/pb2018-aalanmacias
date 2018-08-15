90896d6ea9e08d404c17a32792a79ab4575e83e9
Macias Padilla Brayhan Alan <maciaspb2018@licifug.ugto.mx>
2018-08-15 15:26:00 -0500

    programa operaciones terminado

--- semana2/programa_operaciones.c
+++ semana2/programa_operaciones.c
@@ -12,28 +12,32 @@ e=a+(b+c)/d */
    int a, b, c, d;
    int resultado1, resultado2, resultado3, resultado4;  
   
-   printf("vamos a realizar algunas operaciones. Tendras que introducir 4 valores\n");
+   printf("vamos a realizar algunas operaciones. Tendras que introducir 4 valores enteros\n");
    printf("introduce el valor de a\n");
-   scanf("%i", a);
+   scanf("%i", &a);
    printf("introduce el valor de b\n");
-   scanf("%i", b);
+   scanf("%i", &b);
    printf("introduce el valor de c\n");
-   scanf("%i", c);
+   scanf("%i", &c);
    printf("introduce el valor de d\n");
-   scanf("%i", d); 
+   scanf("%i", &d); 
+
 
    printf("ahora realizaremos la sigiente operación: e=(a+b)*c/d\n");
-     resultado1=(a+b)*c/d;
-   printf("el resultado es %i", resultado1);
+     resultado1 = ((a+b)*c/d);
+   printf("el resultado es %i\n",resultado1);
 
    printf("ahora realizaremos la sigiente operación: e=((a+b)*c)/d\n");
-     resultado2=((a+b)*c)/d
-   printf("el resultado es %i", resultado2);
+     resultado2 = (((a+b)*c)/d);
+   printf("el resultado es %i\n",resultado2);
 
    printf("ahora realizaremos la sigiente operación: e=(a+b)*c/d\n");
-     resultado3=(a+b)*c/d;
-   printf("el resultado es %i", resultado3);
+     resultado3 = ((a+b)*c/d);
+   printf("el resultado es %i\n",resultado3);
 
    printf("ahora realizaremos la sigiente operación: e=a+(b+c)/d\n");
-     resultado4=a+(b+c)/d;
-   printf("el resultado es %i", resultado4);
+     resultado4 = (a+(b+c)/d);
+   printf("el resultado es %i\n",resultado4);
+
+  return 0;
+}
